66a2249464cfd1bd8b394c9140250ef873757a54
GTDominic <seitheldominic@gmail.com>
2021-01-08 10:19:51 +0100

    Email mit validation

--- src/app/registrierung/registrierung.component.ts
+++ src/app/registrierung/registrierung.component.ts
@@ -1,4 +1,6 @@
+import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import {ErrorStateMatcher} from '@angular/material/core';
 
 @Component({
   selector: 'app-registrierung',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrierung.component.css']
 })
 export class RegistrierungComponent implements OnInit {
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+}
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
 }
