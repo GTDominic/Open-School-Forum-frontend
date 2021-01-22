2b068ea88c0ac98a752613e15f3cd3068235b1d6
Earlysator <dennis.stuerze@gmx.net>
2021-01-22 09:57:52 +0100

    Hinzufügen von routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { ThreadlistComponent } from './threadlist/threadlist.component';
+import { NewthreadComponent } from './newthread/newthread.component';
 import { AnmeldungComponent } from './anmeldung/anmeldung.component';
 import { RegistrierungComponent } from './registrierung/registrierung.component';
 import { NgModule } from '@angular/core';
@@ -6,7 +8,9 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'registrierung', component: RegistrierungComponent},
-  { path: 'anmeldung', component: AnmeldungComponent}
+  { path: 'anmeldung', component: AnmeldungComponent},
+  { path: 'threads/new', component: NewthreadComponent},
+  { path: 'threads/list', component: ThreadlistComponent}
 ];
 
 @NgModule({
