e452e2de969ad650c13cc5334a40c058d0778c1a
GTDominic <seitheldominic@gmail.com>
2021-02-06 10:31:08 +0100

    newthread routes to thread

--- src/app/newthread/newthread.component.ts
+++ src/app/newthread/newthread.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ThreadserviceService } from './../threadservice.service';
 import { Component, OnInit } from '@angular/core';
@@ -9,12 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewthreadComponent implements OnInit {
 
-  constructor(public threadservice: ThreadserviceService) { }
+  constructor(
+    public threadservice: ThreadserviceService,
+    private router: Router
+  ) { }
 
   ThreadForm = new FormGroup({
     Titel: new FormControl('', [Validators.required, Validators.minLength(4)]),
     Post: new FormControl('', [Validators.required, Validators.minLength(20)])
   });
+
+  dat: any;
 
   ngOnInit() {
   }
@@ -29,7 +35,8 @@ export class NewthreadComponent implements OnInit {
       this.threadservice.createThread(NewThread)
       .subscribe(
         data => {
-          console.log(data);
+          this.dat = data;
+          this.router.navigate([`thread/${this.dat.id}`]);
         }
       );
     }
