dd79d4ccece387bb0759c3a189c4f868c836134b
GTDominic <seitheldominic@gmail.com>
2021-02-06 10:31:28 +0100

    send new message in thread

--- src/app/thread/thread.component.ts
+++ src/app/thread/thread.component.ts
@@ -1,3 +1,5 @@
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UserserviceService } from './../userservice.service';
 import { ThreadserviceService } from './../threadservice.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -11,16 +13,41 @@ export class ThreadComponent implements OnInit {
 
   constructor(
     public threadservice: ThreadserviceService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    public userservice: UserserviceService
   ) { }
+
+  NewPostForm = new FormGroup({
+    Titel: new FormControl(''),
+    Post: new FormControl('', [Validators.required, Validators.minLength(20)])
+  });
 
   threadData: any;
 
   ngOnInit() {
+    this.fetchData();
+  }
+
+  fetchData() {
     this.threadservice.getThreadWtihPosts(this.route.snapshot.params.id)
       .subscribe(data => {
         this.threadData = data;
       });
+  }
+
+  onNewPostSubmit(): void {
+    if ( this.NewPostForm.valid === true ) {
+      const NewPost = {
+        ptitle: this.NewPostForm.get('Titel').value,
+        pmessage: this.NewPostForm.get('Post').value
+      };
+      this.threadservice.newPost(NewPost, this.route.snapshot.params.id)
+      .subscribe(
+        data => {
+          this.fetchData();
+        }
+      );
+    }
   }
 
 }
